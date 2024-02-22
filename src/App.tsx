2cab9bd98515ab359ff45a9d8a617b87e39296f2
Vidoje Zeljic <zeljicvidoje@gmail.com>
2024-02-23 00:34:38 +0100

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Button from "@mui/material/Button";
+import Header from "./components/Header";
+import {Grid} from "@mui/material";
 
 export default function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
-            <h1>VIT</h1>
-            <div className="card">
+            <Header/>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                sx={{ minHeight: '10vh' }}
+            >
                 <Button
                     variant="contained"
                     onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </Button>
-            </div>
+            </Grid>
         </>
     )
 }
