d9d79c055a6030bf1e955bc9149504762839f4b4
Vidoje Zeljic <zeljicvidoje@gmail.com>
2024-02-23 13:41:12 +0100

    add axios

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,7 +1,30 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+// const baseURL = "https://dummyjson.com/products";
+
 export default function Products() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                setData(response.data);
+                console.log("foo")
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
     return (
         <>
-            <h1>Products</h1>
+            Products
+            <ul>
+                {data.map(item => (
+                    <li key={item.id}>{item.title}</li>
+                ))}
+            </ul>
         </>
     );
 }
